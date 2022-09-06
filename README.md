1b3d3520a448fcede175568245c0ea158b4a537b
wenjie132 <zzaaqqwwddcc@foxmail.com>
2022-09-06 10:18:26 +0800

    up

---
+++ README.md
@@ -0,0 +1,11 @@
+#JAVA黄金矿工小游戏
+##项目结构：
+###·Bg类，背景类；
+###·GameWin类，游戏的主类；
+###·Gold类，金块类；
+###·Line类，线类；
+###·Object类，物品父类；
+###·Rock类
+
+
+资料来源：https://www.bilibili.com/video/BV1n34y1t7EZ?p=28&vd_source=0603b76713949987efcb6f8efb081115
---
+++ src/com/sxt/musicStuff.java
@@ -0,0 +1,34 @@
+package com.sxt;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class musicStuff {
+    void playMusic(String musicLocation)
+    {
+        try
+        {
+            File musicPath = new File(musicLocation);
+
+            if(musicPath.exists())
+            {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            else
+            {
+
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+}
